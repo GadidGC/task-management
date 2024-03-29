3571861db075454d9e5efee6063e83577f4a1764
Gabriel Gonza <gabrieldgc1999@gmail.com>
2024-03-29 02:10:17 -0500

    build: test apollo client

---
+++ lib/apollo-wrapper.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import {
+    ApolloLink,
+    HttpLink
+} from "@apollo/client";
+import {
+    ApolloNextAppProvider,
+    NextSSRApolloClient,
+    NextSSRInMemoryCache,
+    SSRMultipartLink,
+} from "@apollo/experimental-nextjs-app-support/ssr";
+
+function makeClient() {
+  const httpLink = new HttpLink({
+      // https://studio.apollographql.com/public/spacex-l4uc6p/
+      uri: "https://syn-api-prod.herokuapp.com/graphql",
+      headers: {
+        authorization: `Bearer ${process.env.TOKEN}`,
+      }
+  });
+
+  return new NextSSRApolloClient({
+    cache: new NextSSRInMemoryCache(),
+    link:
+      typeof window === "undefined"
+        ? ApolloLink.from([
+            new SSRMultipartLink({
+              stripDefer: true,
+            }),
+            httpLink,
+          ])
+        : httpLink,
+  });
+}
+
+export function ApolloWrapper({ children }: React.PropsWithChildren) {
+  return (
+    <ApolloNextAppProvider makeClient={makeClient}>
+      {children}
+    </ApolloNextAppProvider>
+  );
+}
