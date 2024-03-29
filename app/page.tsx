753ea13ace22dfb08a0d84a1d9b3d323a8bd0ecc
Gabriel Gonza <gabrieldgc1999@gmail.com>
2024-03-29 03:38:25 -0500

    build: tanstack table

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { DataTableDemo } from "@/components/bucket";
 import { PollPage } from "./test";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <PollPage/>
+      <DataTableDemo/>
     </main>
   );
 }
